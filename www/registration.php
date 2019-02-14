cdbe81acd4b279bea1c22ebcaed7e64613cd1a67
Hunter <thecryptohunter@protonmail.com>
2019-02-14 15:31:16 +0000

    Update

--- www/registration.php
+++ www/registration.php
@@ -1,27 +1,59 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Google reCAPTCHA v3</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-    <script src="https://www.google.com/recaptcha/api.js?render=6LeI6pAUAAAAAPdCgVajKzU4VoxQ3GLKg9A1gjlP"></script>
-    <script>
+	<head>
+		<title>Redstone Platform</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://www.google.com/recaptcha/api.js?render=6LeI6pAUAAAAAPdCgVajKzU4VoxQ3GLKg9A1gjlP"></script>
+        <script>
         grecaptcha.ready(function () {
             grecaptcha.execute('6LeI6pAUAAAAAPdCgVajKzU4VoxQ3GLKg9A1gjlP', { action: 'contact' }).then(function (token) {
                 var recaptchaResponse = document.getElementById('recaptchaResponse');
                 recaptchaResponse.value = token;
             });
         });
-    </script>
-</head>
+        </script>	
+    </head>
+	<body class="is-preload">
 
-<body>
-    <section class="section">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-half">
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">	
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">Redstone Platform</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>					
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="gettingstarted.html">Download wallet</a></li>
+								<li><a href="assets/Redstone_Whitepaper.pdf">Download Whitepaper</a></li>
+								<li><a href="http://test.blockexplorer.redstoneplatform.com/block-explorer">Block Explorer (Testnet)</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon fa-diamond" style='font-size:48px;color:red'></span></div>
+							<h2>This is Redstone</h2>
+							<p></p>
+					</div>
+
+					</section>
 
                     <?php // Check if form was submitted:
                     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
@@ -40,48 +72,69 @@
                         }
                     } ?>
 
-                    <form method="POST">
+			<!-- Footer -->
+            <section id="footer">
+						<div class="inner">
+							<h2>Airdrop Registration</h2>
+							<p>To register for the <strong>Redstone</strong> airdrop, please provide the following details.</p>
 
-                        <h1 class="title">
-                            reCAPTCHA v3 example
-                        </h1>
+                            <form method="post" action="/php/registration.php">
+								<div class="fields">
+									<div class="field">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name" required autofocus />
+									</div>
+									<div class="field">
+										<label for="email">Email</label>
+										<input type="email" name="email" id="email" required />
+									</div>
+									<div class="field">
+										<label for="redstone_add">Redstone Address</label>
+										<input type="text" name="redstone_add" id="redstone_add" required />
+									</div>
+									<div class="field">
+										<label for="github">Github Name</label>
+										<input type="text" name="github" id="github" required />
+									</div>
+									<div class="field">
+										<label for="discord">Redstone Discord Id</label>
+										<input type="text" name="discord" id="discord" required />
+									</div>
+									<div class="field">
+										<label for="telegram">Redstone Telegram Id</label>
+										<input type="text" name="telegram" id="telegram" required />
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="4"></textarea>
+									</div>
+								</div>
+								<ul class="actions">				
+                                    <li><input type="submit" value="Register" /></li>
+								</ul>
+                                    <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
+							</form>
+							<br>
+							<div class="footer-social-icons">
+							<br><h2>Follow us on</h2>
+							<a href="https://github.com/redstoneplatform" class="social-icon"><i class="fab fa-github"></i></a>
+							<a href="https://t.me/redstoneplatform"" class="social-icon"><i class="fab fa-telegram"></i></a>
+							<a href="https://discord.gg/BCSX854" class="social-icon"><i class="fab fa-discord"></i></a>
+							<a href="https://twitter.com/redstonecoin" class="social-icon"><i class="fab fa-twitter"></i></a>
+							<a href="mailto:admin@redstonecoin.com" class="social-icon"><i class="fa fa-at"></i></a>
+							</div>
+							<ul class="copyright">
+								<li>&copy; Redstone Platform. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+					</section>
+			</div>
 
-                        <div class="field">
-                            <label class="label">Name</label>
-                            <div class="control">
-                                <input type="text" name="name" class="input" placeholder="Name" required>
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label class="label">Email</label>
-                            <div class="control">
-                                <input type="email" name="email" class="input" placeholder="Email Address" required>
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label class="label">Message</label>
-                            <div class="control">
-                                <textarea name="message" class="textarea" placeholder="Message" required></textarea>
-                            </div>
-                        </div>
-
-                        <div class="field is-grouped">
-                            <div class="control">
-                                <button type="submit" class="button is-link">Send Message</button>
-                            </div>
-                        </div>
-
-                        <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
-
-                    </form>
-
-                </div>
-            </div>
-        </div>
-    </section>
-
-</body>
-
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+	</body>
 </html>
